c8b1276c471bed69c68f30e28949913198eb4aeb
amrutha10499 <amrutha.xworkz@gmail.com>
2021-11-30 14:41:53 +0530

    web

---
+++ js1.js
@@ -0,0 +1,17 @@
+<!doctype>
+<html>
+<head>
+<title>js</title>
+<script type="text/javascript">
+function PrintHello(name){
+console.log(name);
+if (name=="lays"){
+alert("lays is of 10rs");
+}
+}
+</script>
+</head>
+<body>
+<button onclick="printhello('lays')"lays</button>
+</body>
+</html>
